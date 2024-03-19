9ffab627a02cde9eaf7d6b1408f0fd94bcc101f0
vanessalagomachado <vanessalagomachado@gmail.com>
2024-03-19 18:14:26 -0300

    Criação do Projeto

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/cs/LPOO_ProjetoCS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifsul.cc.lpoo.cs;
+
+/**
+ *
+ * @author vanessalagomachado
+ */
+public class LPOO_ProjetoCS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
